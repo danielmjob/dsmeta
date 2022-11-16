4ccecdae7a4d727b54a769753849eee3831846ef
danielmjob <danielmarquesjob@gmail.com>
2022-11-16 19:15:12 -0300

    alteração sms

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,7 +37,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" +sale.getDate().getYear();
 		
-		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
